bdde57b9a1cff55b4781be251d6227008336191f
Sergio Alva <sergio_alva99@hotmail.com>
2021-05-28 17:00:24 -0500

    feat: Modify home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {OrganizersComponent} from './pages/organizers/organizers.component';
-import {PlayerHomePageComponent} from './pages/player-home-page/player-home-page.component';
-import {OrganizerHomePageComponent} from './pages/organizer-home-page/organizer-home-page.component';
 import {TeamsComponent} from './pages/teams/teams.component';
 import {AddTeamplayerComponent} from './pages/add-teamplayer/add-teamplayer.component';
-import {LoginComponent} from './pages/login/login.component';
 import {RegisterComponent} from './pages/register/register.component';
 import {ProfileComponent} from './pages/profile/profile.component';
 import {AppComponent} from './app.component';
@@ -14,13 +11,10 @@ import {OrganizerTournamentComponent} from './pages/organizer-tournament/organiz
 import {TournamentPlayerAssingComponent} from './pages/tournament-player-assing/tournament-player-assing.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'playerHomePage', component: PlayerHomePageComponent },
-  { path: 'organizerHomePage', component: OrganizerHomePageComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'teams', component: TeamsComponent },
   { path: 'add-teamplayer', component: AddTeamplayerComponent },
   { path: 'organizers', component: OrganizersComponent},
-  {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent },
   {path: 'profile', component: ProfileComponent},
   { path: 'organizers/:id/free-tournaments', component: TournamentComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,24 +11,21 @@ import { TeamsComponent } from './pages/teams/teams.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { PlayerHomePageComponent } from './pages/player-home-page/player-home-page.component';
-import { OrganizerHomePageComponent } from './pages/organizer-home-page/organizer-home-page.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { AddTeamplayerComponent } from './pages/add-teamplayer/add-teamplayer.component';
 import { authInterceptorProviders } from './helpers/auth.interceptor';
-import { LoginComponent } from './pages/login/login.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { RegisterComponent } from './pages/register/register.component';
 import {MatListModule} from '@angular/material/list';
 import {MatInputModule} from '@angular/material/input';
-import {MatPaginatorModule} from "@angular/material/paginator";
-import {MatTableModule} from "@angular/material/table";
 import {TournamentComponent} from './pages/tournament/tournament.component';
 import {OrganizerTournamentComponent} from './pages/organizer-tournament/organizer-tournament.component';
 import { TournamentPlayerAssingComponent } from './pages/tournament-player-assing/tournament-player-assing.component';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -37,10 +34,7 @@ import { TournamentPlayerAssingComponent } from './pages/tournament-player-assin
     PlayersComponent,
     OrganizersComponent,
     TeamsComponent,
-    PlayerHomePageComponent,
-    OrganizerHomePageComponent,
     AddTeamplayerComponent,
-    LoginComponent,
     ProfileComponent,
     RegisterComponent,
     TournamentComponent,
